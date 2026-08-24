717f129c9438863850a206669e9ef7ade83fd8b8
nolhandespres14-dotcom <nolhandespres14@gmail.com>
2026-08-24 17:05:52 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 // Configuration Supabase de Poképléx
 // Remplace les deux valeurs ci-dessous avec celles de ton projet Supabase.
-export const SUPABASE_URL = '';
-export const SUPABASE_ANON_KEY = '';
+export const SUPABASE_URL = 'https://cllatlnprvrmhxbmvoep.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_-AJsn5lNLn8ul8lbWHy-hw__-HeAYEi';
